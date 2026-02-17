97f943d342af6f9d7aa825a33e76d004a1f6517a
Dhruva <dhruvatajapara@gmail.com>
2026-02-17 16:13:53 +0530

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 
+const cors = require("cors")
+
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const users = [
     {uid : 108568 , name : "Dhruva", att : 80 , total_sub : 15 , bouns : 20 },
